90a0774b312e67f8bc01da571d6bb8cf65510c2c
Alejandro Bryan <alejandrobryan023@gmail.com>
2023-02-10 11:34:23 +0100

    middleware configurations

--- src/app.js
+++ src/app.js
@@ -6,7 +6,11 @@ import ejs from 'ejs';
 import engine from 'ejs-mate';
 import cors from 'cors';
 import { check, validationResult } from 'express-validator';
-import connect_db from '../config/database';
+import passport from 'passport';
+import './passport';
+import auth from './auth';
+import faviconHandler from './utils/utilities/faviconHandler';
+import connect_db from './config/database';
 import exceptionHandler from './utils/exceptions/exceptionHandler';
 import moviesRoute from './api/routes/moviesRoute';
 import usersRoute from './api/routes/usersRoute';
@@ -14,38 +18,26 @@ import genresRoute from './api/routes/genresRoute';
 import directorsRoute from './api/routes/directorsRoute';
 
 import docuRoute from './web/routes/docuRoute';
+import corsOptions from './utils/utilities/corsOptions';
 
 const app = express();
 //connect to the database
 connect_db();
 // require your built in module
 
-let allowedOrigins = ['http://localhost:5000', 'http://testsite.com'];
 // invoking middleware
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  }),
-);
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('common'));
+app.use(faviconHandler);
 app.use(express.static('./public'));
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-import auth from './auth';
+
 auth(app);
-import passport from 'passport';
-import './passport';
+
 app.get('/', (req, res) => {
   res.send(`<h1>Welcome to My-flix </h1> ${'\n'} <a  href="documentation.html">Documentation</a`);
 });
